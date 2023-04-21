19f45d6517e1819d5ba08df4a0c695e824dae9ef
Manuel Avila Rodriguez <95777266+manuelrod98@users.noreply.github.com>
2023-04-21 17:06:09 -0600

    Se agrego musica

--- main.ts
+++ main.ts
@@ -10,10 +10,14 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     testPlayer.y += 5
 })
+let mySprite: Sprite = null
 let testPlayer: Sprite = null
-scene.setBackgroundImage(assets.image`bg-2`)
 testPlayer = sprites.create(assets.image`player-1`, SpriteKind.Player)
 testPlayer.setPosition(80, 60)
+scene.cameraFollowSprite(mySprite)
+game.onUpdate(function () {
+    music.play(music.stringPlayable("B A G F G A B G ", 60), music.PlaybackMode.UntilDone)
+})
 game.onUpdateInterval(500, function () {
 	
 })
