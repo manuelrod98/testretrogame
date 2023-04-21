b76815c02df81712ae85e0ab7d41822bbb9da91d
Manuel Avila Rodriguez <95777266+manuelrod98@users.noreply.github.com>
2023-04-21 16:44:57 -0600

    Se agregaron más recursos y se corrigieron algunos errores.

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-let testPlayer = sprites.create(assets.image`
-    player-1-img
-`, SpriteKind.Player)
-scene.setBackgroundImage(assets.image`
-    bg-1
-`)
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    testPlayer.x += 5
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    testPlayer.x += -5
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    testPlayer.y += -5
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    testPlayer.y += 5
+})
+let testPlayer: Sprite = null
+scene.setBackgroundImage(assets.image`bg-2`)
+testPlayer = sprites.create(assets.image`player-1`, SpriteKind.Player)
 testPlayer.setPosition(80, 60)
+game.onUpdateInterval(500, function () {
+	
+})
