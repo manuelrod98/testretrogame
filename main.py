2627e76353e20f1f0432613cb52060e501fd9f08
Manuel Avila Rodriguez <95777266+manuelrod98@users.noreply.github.com>
2023-04-21 15:59:15 -0600

    Archivos iniciales del proyecto de MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+testPlayer = sprites.create(assets.image("""
+    player-1-img
+"""), SpriteKind.player)
+scene.set_background_image(assets.image("""
+    bg-1
+"""))
+testPlayer.set_position(80, 60)
---
+++ main.ts
@@ -0,0 +1,7 @@
+let testPlayer = sprites.create(assets.image`
+    player-1-img
+`, SpriteKind.Player)
+scene.setBackgroundImage(assets.image`
+    bg-1
+`)
+testPlayer.setPosition(80, 60)
